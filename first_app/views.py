c7b263b37d366b7944b4c2ebc14cfe3cc83200ec
Artur Kawula <kawulaartur@gmail.com>
2020-06-08 22:14:11 +0200

    Adding models, creating superuser

--- first_app/views.py
+++ first_app/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me':"Hello, I am from first_app/index.html!"}
+    my_dict = {'insert_me':"Hello, I am from first_app/index.html!",'insert_two':'secondary text'}
     return render(request,'first_app/index.html',context=my_dict)
 
