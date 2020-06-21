89acc764e15b54daa5c8369da366a5eca937259e
Artur Kawula <kawulaartur@gmail.com>
2020-06-21 20:26:32 +0200

    Add forms. Create templates. Update urls.py.

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic,Webpage,AccessRecord
+from . import forms
 
 # Create your views here. b
 
@@ -10,3 +11,18 @@ def index(request):
     my_dict = {'insert_me':"Hello, I am from first_app/index.html!",'insert_two':'secondary text'}
     return render(request,'first_app/index.html',context=date_dict)
 
+def form_name_view(request):
+    form = forms.FormName()
+
+    if request.method=='POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            #do sth
+            print("VALIDATION SUCCES!")
+            print("NAME: "+form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+
+
+    return render(request,'first_app/form_page.html',{'form':form})
--- first_project/urls.py
+++ first_project/urls.py
@@ -20,6 +20,6 @@ from first_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('first_app/',include('first_app.urls')),
     path('admin/', admin.site.urls),
+    path('form_page/',views.form_name_view, name='form_name'),
 ]
