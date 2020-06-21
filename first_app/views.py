d608eb596bb756320ea241262e74e1ae7f09cf12
Artur Kawula <kawulaartur@gmail.com>
2020-06-21 19:34:35 +0200

    C

--- first_app/views.py
+++ first_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic,Webpage,AccessRecord
 
-# Create your views here.
+# Create your views here. b
 
 def index(request):
     webpages_list = AccessRecord.objects.order_by('date')
