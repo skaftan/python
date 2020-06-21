4b95ed9c286cb04cf842025dc7dd2970a0e0e6e1
Artur Kawula <kawulaartur@gmail.com>
2020-06-21 20:45:11 +0200

    Create email verification.

--- first_app/forms.py
+++ first_app/forms.py
@@ -8,6 +8,13 @@ def check_for_z(value):
 class FormName(forms.Form):
     name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
     
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email !=vmail:
+            raise forms.ValidationError("make sure emails match")
